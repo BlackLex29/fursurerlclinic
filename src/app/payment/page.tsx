fd7683c0056da6b04cad3935378a0525dab30906
BlackLex29 <lextermilo@gmail.com>
2025-08-29 22:15:34 +0845

    new key deploy

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -2,7 +2,7 @@ import React,{ Suspense } from "react";
 import Payment from "@/rlclinic/payment";
 
 export default function Home() {
-  return ( <Suspense fallback={<div>Loading...</div>}>
+  return ( <Suspense fallback={<div>Loading.....</div>}>
       
       <Payment/>
     
